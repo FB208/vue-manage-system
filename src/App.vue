2206e03ecd8ce17547d7d45d4258e0a854ef5533
yhc in cbhb <472189065@qq.com>
2019-07-03 17:11:55 +0800

    引入global.less样式并优化全局滚动条样式

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 </template>
 <style>
     @import "./assets/css/main.css";
-    @import "./assets/css/color-dark.css";     /*深色主题*/
-    /*@import "./assets/css/theme-green/color-green.css";   浅绿色主题*/
+    /*@import "./assets/css/color-dark.css";     深色主题*/
+    @import "./assets/css/theme-green/color-green.css"; /*  浅绿色主题*/
+    /* 新增的样式文件，用less编写，以后main.css逐渐过渡到global.css */
+    @import "./assets/css/global.less"
 </style>

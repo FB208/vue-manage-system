58f36582951d3e98f4f0f420179165513e014547
yhc in cbhb <472189065@qq.com>
2019-07-03 17:45:01 +0800

    none

--- src/components/page/Form/RichForm.vue
+++ src/components/page/Form/RichForm.vue
@@ -1,4 +1,12 @@
 <template>
+<div>
+    <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-lx-calendar"></i> 表单</el-breadcrumb-item>
+                <el-breadcrumb-item>基本表单</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+</div>
     <el-form ref="form" :model="formdata" label-width="160px" size="mini">
         <div style="margin:20px;">
             <fieldset>
